bcbbeecf4e2c12b20a60667a2b345bb77282ee86
dkmehra <dkmehra36@gmail.com>
2022-02-22 15:00:20 +0530

    Create sum.cpp
    adding sum.cpp file

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<stdio>
+using namespace std;
+void sum(int a, int b)
+{
+    cout << " sum of the numbers is: " << a+b << endl;
+}
+int main()
+{
+  // adding two number with the help of a function sum.
+  int a = 10;
+  int b = 20;
+  sum(a,b);
+}
